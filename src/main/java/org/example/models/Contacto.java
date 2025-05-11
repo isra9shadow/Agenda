adaf442d70dfb143bc84a17131cd6112a81e8dc6
Israel Molina <isra9movil@hotmail.com>
2025-05-11 23:23:52 +0200

    clase Contacto completa

--- src/main/java/org/example/models/Contacto.java
+++ src/main/java/org/example/models/Contacto.java
@@ -1,4 +1,71 @@
 package org.example.models;
 
 public class Contacto {
+    private String nombre;
+    private String apellidos;
+    private int telefono;
+    private String email;
+    private String notas;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, String apellidos, int telefono, String email, String notas) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;
+        this.notas = notas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", telefono=" + telefono +
+                ", email='" + email + '\'' +
+                ", notas='" + notas + '\'' +
+                '}';
+    }
 }
